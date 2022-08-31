fc540bbc281699494fb14808a21b07e7b75ef541
RubenBoard <klsj9810@gmail.com>
2022-09-01 01:01:02 +0900

    ~7.4

--- src/App.js
+++ src/App.js
@@ -1,35 +1,5 @@
-import { useState, useEffect } from "react";
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers?limit=200")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
-
-  return (
-    <div>
-      <h1>The coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select>
-          {coins.map((coin) => (
-            <option>
-              {coin.name} ({coin.symbol}) :{" "}
-              {Math.floor(coin.quotes.USD.price) * 1300} 원
-            </option>
-          ))}
-        </select>
-      )}
-    </div>
-  );
+  return null;
 }
 
 export default App;
